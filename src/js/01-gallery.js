bf5b45c51217a02127c98e68aaecfee4b40fab00
Volodymyr Velch <velch83@gmail.com>
2022-07-28 21:56:06 +0300

    js-hw-08-task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+// добавляємо днамічно  елменти масиву в розмітку
+
+const list = galleryItems.map(
+  img =>
+    `
+    <a class="gallery__item" href=${img.original}>
+      <img
+        class="gallery__image"
+        src=${img.preview}
+        title = ${img.description}
+        alt=${img.description}
+      />
+    </a>
+  `
+);
+galleryList.insertAdjacentHTML('afterbegin', list.join(''));
+
+// ініціалізуємо SimpleLightbox , та додаємо опції відображення
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  fadeSpeed: '250',
+  scrollZoom: 'true',
+  animationSpeed: '250',
+});
